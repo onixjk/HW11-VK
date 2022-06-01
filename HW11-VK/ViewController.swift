5cd8d598e1302b0b342cd1de6094a6a2de7589af
onixjk <onixjkk@gmail.com>
2022-06-02 01:18:26 +0300

    Create and add stackView and buttons.
    Add createButtonWithLeftImage() method. Create and add buttonWithLeftImageStackView and buttons. Change blue color. Change name darkGrayColor. Add editButtonBackgroundColor and separatorColor. Change background separator color.

--- HW11-VK/ViewController.swift
+++ HW11-VK/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     private lazy var separator: UIView = {
         let view = UIView()
         
-        view.backgroundColor = .gray
+        view.backgroundColor = Color.separatorColor
         
         return view
     }()
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
     private lazy var statusStackView = createStackView(axis: .vertical, alignment: .leading)
     private lazy var statusLabelStackView = createStackView(axis: .horizontal, alignment: .fill)
     private lazy var buttonsWithTopImageStackView = createStackView(axis: .horizontal, alignment: .fill)
+    private lazy var buttonWithLeftImageStackView = createStackView(axis: .vertical, alignment: .leading)
     
     private lazy var nameLabel = createLabel(text: "Имя Фамилия", textColor: .white, fontSize: 22, weight: .medium)
     private lazy var statusLabel = createLabel(text: "online", textColor: .systemGray, fontSize: 16, weight: .regular)
@@ -45,6 +46,12 @@ class ViewController: UIViewController {
     private lazy var recordButton = createButtonWithTopImage(imageName: "square.and.pencil", title: "Запись", color: Color.blueColor)
     private lazy var photoButton = createButtonWithTopImage(imageName: "photo", title: "Фото", color: Color.blueColor)
     private lazy var clipButton = createButtonWithTopImage(imageName: "play", title: "Клип", color: Color.blueColor)
+    
+    private lazy var cityButton = createButtonWithLeftImage(imageName: "house", title: "Город: Москва", color: Color.grayColor)
+    private lazy var subscribersButton = createButtonWithLeftImage(imageName: "dot.radiowaves.up.forward", title: "35 подписчиков", color: Color.grayColor)
+    private lazy var placeOfWorkButton = createButtonWithLeftImage(imageName: "briefcase", title: "Указать место работы",color: Color.blueColor)
+    private lazy var giftButton = createButtonWithLeftImage(imageName: "snowflake", title: "Получить подарок", color: Color.purpleColor)
+    private lazy var infoButton = createButtonWithLeftImage(imageName: "info.circle.fill", title: "Подробная информация", color: .white)
     
     // MARK: - Lifecycle
     
@@ -69,6 +76,7 @@ class ViewController: UIViewController {
         parentStackView.addArrangedSubview(editButton)
         parentStackView.addArrangedSubview(buttonsWithTopImageStackView)
         parentStackView.addArrangedSubview(separator)
+        parentStackView.addArrangedSubview(buttonWithLeftImageStackView)
         
         headerStackView.addArrangedSubview(logo)
         headerStackView.addArrangedSubview(statusStackView)
@@ -84,6 +92,12 @@ class ViewController: UIViewController {
         buttonsWithTopImageStackView.addArrangedSubview(recordButton)
         buttonsWithTopImageStackView.addArrangedSubview(photoButton)
         buttonsWithTopImageStackView.addArrangedSubview(clipButton)
+        
+        buttonWithLeftImageStackView.addArrangedSubview(cityButton)
+        buttonWithLeftImageStackView.addArrangedSubview(subscribersButton)
+        buttonWithLeftImageStackView.addArrangedSubview(placeOfWorkButton)
+        buttonWithLeftImageStackView.addArrangedSubview(giftButton)
+        buttonWithLeftImageStackView.addArrangedSubview(infoButton)
     }
     
     private func setupLayout() {
@@ -129,6 +143,7 @@ class ViewController: UIViewController {
     
     private func setupStackView() {
         parentStackView.spacing = 10
+        buttonWithLeftImageStackView.spacing = 10
     }
     
     private func setupImageView() {
@@ -137,8 +152,10 @@ class ViewController: UIViewController {
     }
     
     private func setupButton() {
-        editButton.backgroundColor = Color.darkGrayColor
+        editButton.backgroundColor = Color.editButtonBackgroundColor
         editButton.layer.cornerRadius = 10
+        
+        infoButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
     }
 
     
@@ -190,6 +207,24 @@ class ViewController: UIViewController {
         
         return button
     }
+    
+    private func createButtonWithLeftImage(imageName: String, title: String, color: UIColor) -> UIButton {
+        
+        let button = UIButton(type: .system)
+        
+        let image = NSTextAttachment()
+        image.image = UIImage(systemName: imageName)?.withTintColor(color)
+        image.bounds = CGRect(x: 0, y: -6, width: 25, height: 25)
+        
+        let resultString = NSMutableAttributedString(attachment: image)
+        resultString.append(NSAttributedString(string: "    "))
+        resultString.append(NSAttributedString(string: title))
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        button.setTitleColor(color, for: .normal)
+        button.setAttributedTitle(resultString, for: .normal)
+        
+        return button
+    }
 }
 
 // MARK: - Constants
@@ -210,8 +245,11 @@ extension ViewController {
     }
     
     enum Color {
-        static let blueColor: UIColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-        static let darkGrayColor = #colorLiteral(red: 0.1415808797, green: 0.1537714303, blue: 0.1702026427, alpha: 1)
+        static let blueColor: UIColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        static let editButtonBackgroundColor = #colorLiteral(red: 0.1415808797, green: 0.1537714303, blue: 0.1702026427, alpha: 1)
+        static let grayColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        static let purpleColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        static let separatorColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 0.7)
     }
 }
 
