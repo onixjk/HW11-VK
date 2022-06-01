f8b0035b05e6f1bbe2c7ba5adce6530c18a298d9
onixjk <onixjkk@gmail.com>
2022-06-01 23:56:04 +0300

    Create and add parentStackView. Setup layout.

--- HW11-VK/ViewController.swift
+++ HW11-VK/ViewController.swift
@@ -1,19 +1,60 @@
-//
-//  ViewController.swift
-//  HW11-VK
-//
-//  Created by Onix qq on 1.06.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var parentStackView = createStackView(axis: .vertical, alignment: .fill)
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupHierarchy()
+        setupLayout()
+        setupView()
     }
+    
+    // MARK: - Settings
+    
+    private func setupHierarchy() {
+        
+        view.addSubview(parentStackView)
+    }
+    
+    private func setupLayout() {
+        
+        parentStackView.translatesAutoresizingMaskIntoConstraints = false
+        parentStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                             constant: Metric.parentStackViewTopOffset).isActive = true
+        parentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                                                 constant: Metric.parentStackViewLeadingOffset).isActive = true
+        parentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                                                  constant: Metric.parentStackViewTrailingOffset).isActive = true
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .black
+    }
+    
+    // MARK: - Privatre Methods
+    
+    private func createStackView(axis: NSLayoutConstraint.Axis, alignment: UIStackView.Alignment) -> UIStackView {
+        let stackView = UIStackView()
+        
+        stackView.axis = axis
+        stackView.alignment = alignment
+        
+        return stackView
+    }
+}
 
+// MARK: - Constants
 
+extension ViewController {
+    enum Metric {
+        static let parentStackViewTopOffset: CGFloat = 15
+        static let parentStackViewLeadingOffset: CGFloat = 10
+        static let parentStackViewTrailingOffset: CGFloat = -10
+    }
 }
 
