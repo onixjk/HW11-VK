6addaaaf943496966719280928a1b88ab20a0592
onixjk <onixjkk@gmail.com>
2022-06-02 00:30:07 +0300

    Create and add stackView, labels, button, image
    Create and add statusLabelStackView, nameLabel, statusLabel, statusButton, phoneImage. Setup layout. Add color.

--- HW11-VK/ViewController.swift
+++ HW11-VK/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     private lazy var logo: UIImageView = {
         let imageView = UIImageView()
         
@@ -10,9 +11,25 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var phoneImage: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(systemName: "iphone")
+        let grayImage = image?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+        
+        imageView.image = grayImage
+        
+        return imageView
+    }()
+    
     private lazy var parentStackView = createStackView(axis: .vertical, alignment: .fill)
     private lazy var headerStackView = createStackView(axis: .horizontal, alignment: .center)
     private lazy var statusStackView = createStackView(axis: .vertical, alignment: .leading)
+    private lazy var statusLabelStackView = createStackView(axis: .horizontal, alignment: .fill)
+    
+    private lazy var nameLabel = createLabel(text: "Имя Фамилия", textColor: .white, fontSize: 22, weight: .medium)
+    private lazy var statusLabel = createLabel(text: "online", textColor: .systemGray, fontSize: 16, weight: .regular)
+    
+    private lazy var statusButton = createButton(title: "Установить статус", titleColor: Color.blueColor, fontSize: 18, weight: .light)
     
     // MARK: - Lifecycle
     
@@ -35,6 +52,13 @@ class ViewController: UIViewController {
         
         headerStackView.addArrangedSubview(logo)
         headerStackView.addArrangedSubview(statusStackView)
+        
+        statusStackView.addArrangedSubview(nameLabel)
+        statusStackView.addArrangedSubview(statusButton)
+        statusStackView.addArrangedSubview(statusLabelStackView)
+        
+        statusLabelStackView.addArrangedSubview(statusLabel)
+        statusLabelStackView.addArrangedSubview(phoneImage)
     }
     
     private func setupLayout() {
@@ -84,6 +108,26 @@ class ViewController: UIViewController {
         
         return stackView
     }
+    
+    private func createLabel(text: String, textColor: UIColor, fontSize: CGFloat, weight: UIFont.Weight) -> UILabel {
+        let label = UILabel()
+        
+        label.text = text
+        label.textColor = textColor
+        label.font = .systemFont(ofSize: fontSize, weight: weight)
+        
+        return label
+    }
+    
+    private func createButton(title: String, titleColor: UIColor, fontSize: CGFloat, weight: UIFont.Weight) -> UIButton {
+        let button = UIButton(type: .system)
+        
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: fontSize, weight: weight)
+        
+        return button
+    }
 }
 
 // MARK: - Constants
@@ -97,6 +141,10 @@ extension ViewController {
         static let logoTrailingOffset: CGFloat = -10
         static let logoHeight: CGFloat = 100
         static let logoWidth: CGFloat = 100
+    }
+    
+    enum Color {
+        static let blueColor: UIColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
     }
 }
 
